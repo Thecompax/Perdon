f14cc9d4f5480b971f90692a6686118a4132a137
Thecompax <acorrales820@gmail.com>
2024-01-07 22:59:51 -0500

    Cambios en el archivo que mejoran la interaccion del no

--- me_perdonas.js
+++ me_perdonas.js
@@ -1,24 +1,26 @@
-function random(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) + min);
-}
-
-let nob = document.getElementById("No");
+let nob = document.getElementById("No")
 
 nob.addEventListener("mousemove", function() {
-    let alto = random(1, 10);
-    let ancho = random(1, 10);
-    console.log(alto, ancho);
+    let alto = random(1,10)
+    let ancho = random(1,10)
+    console.log(alto, ancho)
     nob.style.gridRow = alto;
     nob.style.gridColumn = ancho;
-});
 
-let sib = document.getElementById("Si");
+    })
+
+
+function random(min,max) {
+    let numero = Math.floor(Math.random() * (max - min + 1) + min)
+    return numero
+}
+
+let sib = document.getElementById("Si")
 
 sib.addEventListener("click", function() {
-    let pElement = document.getElementById("p");
-    pElement.removeAttribute("hidden");
-    console.log("click");
+    document.getElementById("p").removeAttribute("hidden")
+    console.log("click")
     document.getElementById("1").style.color = "transparent";
-    sib.style.display = "none";
-    nob.style.display = "none";
-});
+    document.getElementById("Si").style.display = "none";
+    document.getElementById("No").style.display = "none";
+})
